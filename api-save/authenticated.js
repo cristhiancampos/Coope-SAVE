383ac5c195dbfe603027537d9c8cad73df975486
Cristhian Campos <campse21@gmail.com>
2017-10-10 14:39:27 -0600

    archivos para node

---
+++ api-save/authenticated.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'clave_secreta_para_hashear_el_token';
+
+exports.ensureAuth = function(req, res ,next){
+    if(!req.headers.authorization){
+        return res.status(403).send({message: 'La petición no tiene cabecera de autentificación'});
+    }
+
+    var token = req.headers.authorization.replace(/['"]+/g,''); 
+    try {
+        var payload =jwt.decode(token,secret);
+        if(payload.exp <= moment().unix())
+        {
+         res.status(401).send({message: 'El Token ha expirado'});
+        }
+    } catch (error) {
+      //  console.log(error);
+        res.status(404).send({message: 'Token no válido'});
+    }
+    req.user = payload;
+    next();
+};
