e8a7a7c1be9d74401ec4ad9c6357136a02d682c2
Unknown <estban35@hotmail.com>
2017-10-23 14:13:55 -0600

    w

--- api-save/controladores/recurso.js
+++ api-save/controladores/recurso.js
@@ -125,6 +125,35 @@ function modificarRecurso(req, res) {
     });
   }
 
+  function validarModificacion(req, res) {
+    var params = req.body;
+    var nombre = params.nombre;
+    var id = params._id;
+  
+    Sala.findOne({ _id: id, nombre: nombre, estado: { $ne: "Eliminado" } }, (err, sala) => {
+      if (err) {
+        res.status(200).send({ message: null });
+      } else {
+        if (!sala) {
+          Sala.findOne({ nombre: nombre, estado: { $ne: "Eliminado" } }, (err, salaEdit) => {
+            if (err) {
+              res.status(200).send({ message: null });
+            } else {
+              if (!salaEdit) {
+                res.status(200).send({ message: null });
+              }
+              else {
+                res.status(200).send({ message: salaEdit });
+              }
+            }
+          })
+        }
+        else {
+          res.status(200).send({ message: null });
+        }
+      }
+    });
+
 
     module.exports = {
         
