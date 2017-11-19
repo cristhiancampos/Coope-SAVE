4ff7b849a35c1682d5b34e73ba4b1b4a64ce8cbb
Cristhian Campos <campse21@gmail.com>
2017-11-19 15:12:20 -0600

    cambios

--- Coope-SAVE/src/app/app.routing.ts
+++ Coope-SAVE/src/app/app.routing.ts
@@ -1,7 +1,6 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {SolicitudSalaComponent} from './solicitud-sala/solicitud-sala.component';
-import {RegistoUsuarioComponent} from './registo-usuario/registo-usuario.component';
 import {PrincipalComponent} from './principal/principal.component';
 import {ModificarUsuarioComponent} from './modificar-usuario/modificar-usuario.component';
 //admin modules
